229ee031e3e3a033440191c3f681f56e0af5c168
Conor Clarke <conorc470@gmail.com>
2017-12-01 15:00:26 +0000

    settings changed

--- project3/settings.py
+++ project3/settings.py
@@ -16,6 +16,7 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+SITE_ID = 1
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
