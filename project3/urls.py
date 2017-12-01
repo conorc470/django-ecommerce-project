8e20775af1fe153ad232cb78eb2ebb5acd7c6988
Conor Clarke <conorc470@gmail.com>
2017-12-01 14:29:58 +0000

    travis ci

--- project3/urls.py
+++ project3/urls.py
@@ -32,6 +32,7 @@ urlpatterns = [
     url(r'^products/', include(urls_products)),
     url(r'^checkout/', include(urls_checkout)),
     url(r'^cart/', include(urls_cart)),
+    url(r'^user/', include(urls_accounts)),
     url(r'^search/', include(urls_search)),
     url(r'^media/(?P<path>.*)$', static.serve,{'document_root': MEDIA_ROOT}),
 ]
